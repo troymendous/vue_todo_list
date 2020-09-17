73a865f6cfa3cb1fabb5694d5aed68e01bab00ad
Troy Wilson <hello@troywilson.io>
2020-09-17 23:06:36 +1000

    finishing project still one res undefined error

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,39 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo " />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
 import Header from "./components/layout/Header";
-import Todos from "./components/Todos";
-import AddTodo from "./components/AddTodo";
-import axios from "axios";
-
 export default {
-  name: "App",
+  name: "app",
   components: {
     Header,
-    Todos,
-    AddTodo,
-  },
-  data() {
-    return {
-      todos: [],
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    },
-  },
-  created() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => (this.todos = res.data))
-      .catch((err) => console.log(err));
   },
 };
 </script>
 
 <style>
-/* GLobal Syling */
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+#nav {
+  padding: 30px;
 }
 
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.btn:hover {
-  background: #666;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
